24392d22c92ace0688cbf064fb074f831f239a82
Anna <apzhadovets@gmail.com>
2019-03-30 22:30:02 +0300

    добавлен тест отмены удаления руппы

--- tests/test_remove_group.py
+++ tests/test_remove_group.py
@@ -4,6 +4,8 @@ import random
 def test_remove_group(app):
     if app.groups.get_group_list() == 0:
         app.groups.add_new_group("group name")
+        app.groups.add_new_group("group name2")
+        app.groups.add_new_group("group name3")
     old_list = app.groups.get_group_list()
     group = random.choice(old_list)
     move_to_group = random.choice(old_list)
@@ -12,4 +14,16 @@ def test_remove_group(app):
     app.groups.remove_group(group, move_to_group)
     new_list = app.groups.get_group_list()
     old_list.remove(group)
+    assert sorted(old_list) == sorted(new_list)
+
+
+def test_cancel_remove_group(app):
+    if app.groups.get_group_list() == 0:
+        app.groups.add_new_group("group name")
+        app.groups.add_new_group("group name2")
+        app.groups.add_new_group("group name3")
+    old_list = app.groups.get_group_list()
+    group = random.choice(old_list)
+    app.groups.cancel_delete_group(group)
+    new_list = app.groups.get_group_list()
     assert sorted(old_list) == sorted(new_list)
