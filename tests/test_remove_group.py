e97c52e4ab26b6366fd42e6a36fd6028fec700c0
Anna <apzhadovets@gmail.com>
2019-03-30 22:39:27 +0300

    добавлен тест удаления группы вместе с контактами

--- tests/test_remove_group.py
+++ tests/test_remove_group.py
@@ -27,3 +27,16 @@ def test_cancel_remove_group(app):
     app.groups.cancel_delete_group(group)
     new_list = app.groups.get_group_list()
     assert sorted(old_list) == sorted(new_list)
+
+
+def test_remove_with_contact(app):
+    if app.groups.get_group_list() == 0:
+        app.groups.add_new_group("group name")
+        app.groups.add_new_group("group name2")
+        app.groups.add_new_group("group name3")
+    old_list = app.groups.get_group_list()
+    group = random.choice(old_list)
+    app.groups.remove_group_with_contact(group)
+    new_list = app.groups.get_group_list()
+    old_list.remove(group)
+    assert sorted(old_list) == sorted(new_list)
