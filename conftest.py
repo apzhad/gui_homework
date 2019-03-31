9332abb77ff2c4d196d72d765606a48895b605c3
Anna <apzhadovets@gmail.com>
2019-03-31 13:22:31 +0300

    добавлено чтение имен групп их excel файла

--- conftest.py
+++ conftest.py
@@ -1,5 +1,7 @@
 import pytest
 from fixture.application import Application
+from comtypes.client import CreateObject
+import os
 
 
 @pytest.fixture(scope="session")
@@ -7,3 +9,21 @@ def app(request):
     fixture = Application("C:\\Apps\\AddressBook.exe")
     request.addfinalizer(fixture.destroy)
     return fixture
+
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith('xlsx_'):
+            testdata = load_from_xlsx(fixture[5:])
+            metafunc.parametrize(fixture, testdata)
+
+
+def load_from_xlsx(file):
+    xl = CreateObject("Excel.Application")
+    wb = xl.Workbooks.Open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "%s.xlsx" % file))
+    data = []
+    sheet = wb.worksheets[1]
+    for i in range(sheet.UsedRange.Rows.Count):
+        data.append(sheet.Range["A%s" % (i+1)].Value[()])
+    xl.Quit()
+    return data
--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,22 +1,21 @@
-import string
-import random
-import pytest
+import re
 
 
-def random_string(max_length):
-    symbols = string.ascii_letters + string.digits + ' '*10
-    return "".join([random.choice(symbols) for i in range(random.randrange(max_length))])
-
-
-test_data = [random_string(10) for i in range(5)]
-
-
-@pytest.mark.parametrize("group", test_data, ids=[repr(x) for x in test_data])
-def test_add_group(app, group):
+def test_add_group(app, xlsx_groups):
+    group = xlsx_groups
     old_list = app.groups.get_group_list()
     app.groups.add_new_group(group)
     new_list = app.groups.get_group_list()
-    if len(group) == 0:
+    if len(normalize_name(group)) == 0:
         group = "New group"
     old_list.append(group)
     assert sorted(old_list) == sorted(new_list)
+
+
+def normalize_name(name):
+    if name is not None:
+        name = re.sub('\s+', ' ', name)
+        name = name.strip()
+    else:
+        name = ""
+    return name
